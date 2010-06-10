169085b4117e53ca124c533cc9860307facbeb71
Petr <petr@asusbox.(none)>
2010-06-10 14:12:00 +0200

    Typos

--- dsus.py
+++ dsus.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
-# dsus.py
 
-import sys
-import getopt
-import signal
-import os.path
+""" Debian Smart Upload Server """
+
 from glob import iglob
-import time
-import re
-import tempfile
-import hashlib
-import shutil
-import BaseHTTPServer
 from urlparse import urlparse
+from tempfile import mkdtemp
+from shutil import move
+from time import time
+import sys, getopt, signal, os.path
+import re, hashlib
+import BaseHTTPServer
 
-SERVER_ADDRESS = 8000
 SERVER_VERSION = "DSUS/0.1"
 
-options = {
-	"port": SERVER_ADDRESS,
+config = {
+	"port": 8000,
 	"dest": "./",
 	}
 
@@ -41,8 +37,8 @@ class DSUSHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 	server_version = SERVER_VERSION
 
-	dirname = None
-	filename = None
+	dir = None
+	file = None
 	md5 = None
 	length = 0
 
@@ -53,15 +49,15 @@ class DSUSHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 		url = urlparse(self.path)
 		path = os.path.normpath(url.path)
-		self.dirname = os.path.dirname(path)
-		self.filename = os.path.basename(path)
+		self.dir = os.path.dirname(path)
+		self.file = os.path.basename(path)
 
 		# check meta - name, size, etc.
 		if not self.check_meta():
 			return
 
 		# upload file
-		f = open(os.path.join(tempfile.mkdtemp(), self.filename), "w")
+		f = open(os.path.join(mkdtemp(), self.file), "w")
 		f.write(self.rfile.read(self.length))
 		f.close()
 
@@ -69,36 +65,36 @@ class DSUSHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 		if not self.check_content(f.name):
 			return
 
-		# move to proper place
-		shutil.move(f.name, os.path.join(self.dirname, self.filename))
+		# store file
+		move(f.name, os.path.join(self.dir, self.file))
 		self.send_response(200)
 
 	def check_meta(self):
 		"""
-		Check upload meta information.
+		Check file meta information.
 		"""
 		# check filename
-		if not len(self.filename):
+		if not len(self.file):
 			self.send_error(400, 'No filename')
 			return False
 
-		# remove first / from dirname
-		if os.path.isabs(self.dirname):
-			self.dirname = self.dirname[1:]
+		# filter abs dir
+		if os.path.isabs(self.dir):
+			self.dir = self.dir[1:]
 
 		# check dirname
-		if not os.path.isdir(self.dirname):
+		if not os.path.isdir(self.dir):
 			self.send_error(400, 'Bad directory')
 			# TODO allowed dirs check
 			return False
 
 		# existing file?
-		if os.path.exists(os.path.join(self.dirname, self.filename)):
+		if os.path.exists(os.path.join(self.dir, self.file)):
 			self.send_error(400, 'File uploaded allready')
 			return False
 
 		# check if file should be stored
-		if not self.filename.endswith(SIGNED):
+		if not self.file.endswith(SIGNED):
 			if not self.get_checksum():
 				self.send_error(409, 'File not expected')
 				return False
@@ -107,12 +103,11 @@ class DSUSHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 	def check_content(self, filename):
 		"""
-		Check file to be in .changes and having proper size.
+		Check file content.
 		"""
-		if self.filename.endswith(SIGNED):
+		if self.file.endswith(SIGNED):
 			# TODO verify sign
 			print 'TODO verify sign'
-
 		else:
 			f = open(filename, "r")
 			md5 = hashlib.md5()
@@ -126,19 +121,19 @@ class DSUSHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 	def get_checksum(self):
 		"""
-		Get self.file checksum from .changes file.
+		Get checksum for file from .changes.
 		"""
-		pattern = "([0-9a-f]{32}) ([0-9]+) .* %s" % self.filename
+		pattern = "([0-9a-f]{32}) ([0-9]+) .* %s" % self.file
 		reg = re.compile(pattern)
 
-		for changes in iglob(os.path.join(self.dirname, '*' + CHANGES)):
-			if time.time() - os.path.getmtime(changes) > WINDOW:
+		for changes in iglob(os.path.join(self.dir, '*' + CHANGES)):
+			if time() - os.path.getmtime(changes) > WINDOW:
 				continue
 
 			f = open(changes, "r")
 			for line in f:
 				line = line.strip()
-				if not line.endswith(self.filename):
+				if not line.endswith(self.file):
 					continue
 
 				try:
@@ -180,7 +175,7 @@ def run(server_class=BaseHTTPServer.HTTPServer,
 	"""
 	Server routine.
 	"""
-	server_address = ("", options["port"])
+	server_address = ("", config["port"])
 	httpd = server_class(server_address, handler_class)
 
 	# Set signals handles.
@@ -209,7 +204,7 @@ def main(argv):
 			usage()
 			sys.exit()
 		elif opt in ("-p", "--port"):
-			options["port"] = arg
+			config["port"] = int(arg)
 
 	run(handler_class=DSUSHandler)
 
